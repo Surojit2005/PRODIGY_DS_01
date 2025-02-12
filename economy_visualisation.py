3ecaf0b791728ce0800ce36c41e6e5d4cc96d53c
Surojit Paul <surojitpaul389@gmail.com>
2025-02-12 22:31:22 +0530

    economy_visualisation.py

--- economy_visualisation.py
+++ economy_visualisation.py
@@ -2,11 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load dataset
-file_path = r"C:\Users\SUROJIT PAUL\OneDrive\Desktop\PRODIGY INFOTECH\task 1\economy.csv"  # Ensure this file is in the same directory
+
+file_path = r"C:\Users\SUROJIT PAUL\OneDrive\Desktop\PRODIGY INFOTECH\task 1\economy.csv"  
 df = pd.read_csv(file_path)
 
-# Bar Chart: GDP by Country
 plt.figure(figsize=(12, 6))
 sns.barplot(x="GDP (in Trillions USD)", y="Country", data=df, palette="viridis")
 plt.xlabel("GDP (in Trillions USD)")
@@ -14,7 +13,6 @@ plt.ylabel("Country")
 plt.title("Top 15 Economies by GDP (2023)")
 plt.show()
 
-# Histogram: GDP Distribution
 plt.figure(figsize=(10, 5))
 sns.histplot(df["GDP (in Trillions USD)"], bins=10, kde=True, color="blue")
 plt.xlabel("GDP (in Trillions USD)")
